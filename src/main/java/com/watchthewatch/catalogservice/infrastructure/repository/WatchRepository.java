2853f1f28892125be474cc40210b1bc446a3bf6a
Sebastian Falk <sebastian.falk.1986@gmail.com>
2024-11-09 20:39:26 +0100

    Extending JpaRepository instead of CrudRepository, due to custom made method.

--- src/main/java/com/watchthewatch/catalogservice/infrastructure/repository/WatchRepository.java
+++ src/main/java/com/watchthewatch/catalogservice/infrastructure/repository/WatchRepository.java
@@ -1,8 +1,10 @@
 package com.watchthewatch.catalogservice.infrastructure.repository;
 
 import com.watchthewatch.catalogservice.infrastructure.model.Watch;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WatchRepository extends CrudRepository<Watch, String> {
+import java.util.List;
 
+public interface WatchRepository extends JpaRepository<Watch, String> {
+    List<Watch> findByWatchIdIn(List<String> watchIds);
 }
