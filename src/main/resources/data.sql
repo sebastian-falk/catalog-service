aaf58d6768208d78fe1163dec0a2668e6630b62e
Sebastian Falk <sebastian.falk.1986@gmail.com>
2024-11-12 11:30:33 +0100

    Added data seeding for watches in local env

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO watches (watch_id, watch_name, unit_price, discount) VALUES
+    ('001', 'Rolex', 100.00, '3 for 200'),
+    ('002', 'Michael Kors', 80.00, '2 for 120'),
+    ('003', 'Swatch', 50.00, null),
+    ('004', 'Casio', 30.00, null);
